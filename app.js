4e5fae296f839ffdda43acf904b81006357ae621
naqeebahmad212 <144816925+naqeebahmad212@users.noreply.github.com>
2024-01-07 20:34:04 +0500

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ cloudinary.config({
 app.use(express.json())
 app.use(express.urlencoded({extended:true, limit:'16kb'}))
 app.use(cookieParser())
-app.use(cors({origin:`https://mernstack-frontend-phi.vercel.app`,credentials:true}))
+app.use(cors({origin:"*",credentials:true}))
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000,()=>{
