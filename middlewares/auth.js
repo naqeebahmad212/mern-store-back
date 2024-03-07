15c250ec21362afddc0d7967f915cc0ef7f29790
naqeebahmad212 <naqeebahmad212@gmail.com>
2024-03-07 19:15:55 +0500

    now done

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,38 +1,39 @@
-const ApiErrorHandler = require("../utils/apiError")
-const User=require('../models/userModel')
-const jwt=require('jsonwebtoken')
+const ApiErrorHandler = require("../utils/apiError");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
-const isAuthenticated=(req,res,next)=>{
-    const token=req.cookies.jwtToken
-    if(!token){
-        next(new ApiErrorHandler('Login to Access the resource',401))
-    }else{
-        jwt.verify(token,process.env.SECRET,async(err,userInfo)=>{
-            if(err){
-               next(new ApiErrorHandler('Login to Access the resource',401))
-            }else{
-                req.user= await User.findById(userInfo.id)
-                next()
-            }
-        })
+const isAuthenticated = (req, res, next) => {
+  const token = req.cookies.jwtToken;
+  if (!token) {
+    next(new ApiErrorHandler("Login to Access the resource", 401));
+  } else {
+    jwt.verify(token, "ksdjfskdur", async (err, userInfo) => {
+      if (err) {
+        next(new ApiErrorHandler("Login to Access the resource", 401));
+      } else {
+        req.user = await User.findById(userInfo.id);
+        next();
+      }
+    });
+  }
+};
+
+const isUserAuthorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ApiErrorHandler(
+          `Role : ${req.user.role} is not allowed to access this route`,
+          403
+        )
+      );
+    } else {
+      next();
     }
-}
+  };
+};
 
-
-const isUserAuthorize=(...roles)=>{
-    return (req,res,next)=>{
-        if(!roles.includes(req.user.role)){
-            return next  (new ApiErrorHandler(`Role : ${req.user.role} is not allowed to access this route`,403 ))
-
-        }else{
-            next()
-        }
-    }
-}
-
-
-module.exports={
-    
-    isAuthenticated,
-    isUserAuthorize
-}
+module.exports = {
+  isAuthenticated,
+  isUserAuthorize,
+};
