da3959dbcca9c13a880210d80d9eebe685cf5900
naqeebahmad212 <naqeebahmad212@gmail.com>
2024-03-07 15:35:24 +0500

    url direct

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI);
+    await mongoose.connect(
+      "mongodb+srv://soomrush212:ahmadjan1@my-cluster.i0gyfov.mongodb.net/mynew?retryWrites=true&w=majority"
+    );
     console.log(`connected to ${process.env.DB_URI}`);
   } catch (err) {
     console.log(err);
