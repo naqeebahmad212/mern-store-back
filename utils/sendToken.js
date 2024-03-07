ab1e78afa789a2187715652d448c4bef0c88b8c6
naqeebahmad212 <naqeebahmad212@gmail.com>
2024-03-07 15:42:32 +0500

    json

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,9 +1,11 @@
-const jwt=require('jsonwebtoken')
- const  sendToken=(user,res,statusCode)=>{
-    const maxAge= Date.now()+ 24*3*60*60*1000
-    const token=jwt.sign({id: user._id}, process.env.SECRET,{expiresIn:maxAge})
-    res.cookie('jwtToken',token,{httpOnly:true,expiresIn:maxAge}).status(statusCode).json({message:'login',user})
-}
+const jwt = require("jsonwebtoken");
+const sendToken = (user, res, statusCode) => {
+  const maxAge = Date.now() + 24 * 3 * 60 * 60 * 1000;
+  const token = jwt.sign({ id: user._id }, "ksdjfskdur", { expiresIn: maxAge });
+  res
+    .cookie("jwtToken", token, { httpOnly: true, expiresIn: maxAge })
+    .status(statusCode)
+    .json({ message: "login", user });
+};
 
-
-module.exports = sendToken
+module.exports = sendToken;
