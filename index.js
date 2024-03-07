96925e160fb8e2ac80f8544631e73c752c866b3b
naqeebahmad212 <naqeebahmad212@gmail.com>
2024-03-07 14:08:39 +0500

    link updated

--- index.js
+++ index.js
@@ -1,69 +1,64 @@
-const express =require('express')
-const app =express()
-const path=require('path')
-const cloudinary=require('cloudinary')
-const {connectDB}=require('./config/db')
- const dotenv =require('dotenv')
- const cookieParser=require('cookie-parser')
- const cors=require('cors')
- const errorMiddleware=require('./middlewares/error')
- const userRoutes=require('./routes/UserRoutes')
- const productRoutes=require('./routes/productRoutes')
- const paymentRoutes=require('./routes/paymentRoutes')
- const orderRoutes=require('./routes/orderRoutes')
-app.use(cors({origin:"https://mernstack-frontend-phi.vercel.app",credentials:true}));
+const express = require("express");
+const app = express();
+const path = require("path");
+const cloudinary = require("cloudinary");
+const { connectDB } = require("./config/db");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const errorMiddleware = require("./middlewares/error");
+const userRoutes = require("./routes/UserRoutes");
+const productRoutes = require("./routes/productRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+app.use(
+  cors({
+    origin: "https://mern-store-front.vercel.app",
+    credentials: true,
+    methods: ["POST", "GET"],
+  })
+);
 
- dotenv.config({
-    path:'./config/.env'
-})
-
-
+dotenv.config({
+  path: "./config/.env",
+});
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
- process.on("uncaughtException",(err)=>{
-    console.log(`Error: ${err.message}`)
-    console.log('shuntting down the server due to uncaugth')
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("shuntting down the server due to uncaugth");
 
-    process.exit(1)
-
-})
-
- 
-
-
-
+  process.exit(1);
+});
 
 // cloudinary.co
 
-
-app.use(express.json())
-app.use(express.urlencoded({extended:true, limit:'16kb'}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(`listening to port : ${process.env.PORT} `)
-    })
-})
-.catch((err)=>{
-    console.log(err)
-})
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`listening to port : ${process.env.PORT} `);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-
-app.use(userRoutes)
-app.use(productRoutes)
-app.use(paymentRoutes)
-app.use(orderRoutes)
+app.use(userRoutes);
+app.use(productRoutes);
+app.use(paymentRoutes);
+app.use(orderRoutes);
 
 // app.use(express.static(path.join(__dirname, '../frontend/build')))
 // app.get('*',(req,res)=>{
 //     res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
 // })
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
